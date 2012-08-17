ed94316ea9c644bbd35da06be096776a29c6b8f1
danieru <danieru.dressler@gmail.com>
2012-08-16 19:44:21 -0600

    Fix rails server not running

--- getwordsearches/config/initializers/secret_token.rb
+++ getwordsearches/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Myapp::Application.config.secret_token = '555440e73e608609ec59ae42fddc051150cc2951bac49ea8fcd0bb15f5533629ac6aae566fca66ac7b6b225f990ab37d4cbe0b3eda8c73b45f4f7f69b5766af2'
+Getwordsearches::Application.config.secret_token = '30dd92e52aa9d862920a63961b161d1d9a8215c0c759a6875c09cbbfc27810fb6eb2206071cbbaeee209ce9df6d2c0f24c977bcc633315cb6605b1fdb12a5a1d'
--- getwordsearches/config/routes.rb
+++ getwordsearches/config/routes.rb
@@ -1,4 +1,4 @@
-Myapp::Application.routes.draw do
+Getwordsearches::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +48,7 @@ Myapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+	root :to => 'index.html.erb'
 
   # See how all your routes lay out with "rake routes"
 
